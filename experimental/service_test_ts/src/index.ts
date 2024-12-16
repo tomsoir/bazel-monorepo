788c24936b573572f097be7ab77c86daf67331a1
artem <tomsoir@gmail.com>
2024-12-16 14:34:57 -0800

    Build simple ts project

--- experimental/service_test_ts/src/index.ts
+++ experimental/service_test_ts/src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express';
+// import express from 'express';
 
-const app = express();
+// const app = express();
 
-app.get('/health', (req, res) => {
-    res.status(200).send('helloworld');
-});
+// app.get('/health', (req, res) => {
+//     res.status(200).send('helloworld');
+// });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
+
+console.log(`Hello!`);
