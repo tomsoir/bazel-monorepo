dcbc173ec62121754da759249201f7434a784075
artem <tomsoir@gmail.com>
2024-12-23 18:01:47 -0800

    Fix tomsoir-monorepo/experimental/service_test_ts_webpack, add webpack_app target

--- experimental/service_test_ts_webpack/webpack.config.cjs
+++ experimental/service_test_ts_webpack/webpack.config.cjs
@@ -2,14 +2,10 @@ const path = require('path');
 
 module.exports = {
   // Webpack entry point for the already transpiled JS
-  // entry: './experimental/service_test_ts_webpack/dist/index.js',
   entry: path.resolve(__dirname, 'dist', 'index.js'),
-
   output: {
-    filename: 'bundle1112.js',  // Output bundled file
+    filename: 'webpack_build.js',  // Output bundled file
     path: path.resolve(__dirname, 'dist'),
-    // path: './dist',
-    // path: path.resolve(__dirname, 'dist'),  // Output directory
   },
   resolve: {
     extensions: ['.js'],  // Resolve JavaScript files only
