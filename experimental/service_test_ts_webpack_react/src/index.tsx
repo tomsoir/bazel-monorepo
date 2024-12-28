695b61e609b421956cd78b847f39f352a4978c61
artem <tomsoir@gmail.com>
2024-12-28 04:21:18 -0800

    Add webpack_devserver example

--- experimental/service_test_ts_webpack_react/src/index.tsx
+++ experimental/service_test_ts_webpack_react/src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import util from './util1.js';
 
+// import './style.scss';
+
 const container = document.getElementById('root');
 const root = container && createRoot(container);
 
