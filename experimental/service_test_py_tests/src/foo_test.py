aaf9d7b5d0cb7b8212d418710bfff54c5b854209
artem <tomsoir@gmail.com>
2025-01-24 01:05:27 -0800

    Add PY TESTS support

---
+++ experimental/service_test_py_tests/src/foo_test.py
@@ -0,0 +1,14 @@
+import pytest
+import json
+
+from foo import add
+
+
+def test_add():
+  assert add(1, 1) == 2, "Expected 1 + 1 to equal 2"
+
+
+def test_hello_json():
+  content = open("src/fixtures/hello.json", "r").read()
+  data = json.loads(content)
+  assert data["message"] == "Hello, world.", "Message is as expected"
