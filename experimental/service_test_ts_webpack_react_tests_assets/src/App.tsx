28db4e2a5ac83878f9f78a937c7c10bb9172f09f
artem <tomsoir@gmail.com>
2025-03-14 18:02:53 -0700

    Fix assets example

--- experimental/service_test_ts_webpack_react_tests_assets/src/App.tsx
+++ experimental/service_test_ts_webpack_react_tests_assets/src/App.tsx
@@ -1,13 +1,13 @@
 const App = () => {
-  // const imageURL = new URL('../assets/images/image1.png', import.meta.url).href;
-  // const icon1URL = new URL('../assets/icons/icon1.svg', import.meta.url).href;
-  // const icon2URL = new URL('../assets/icons/icon2.svg', import.meta.url).href;
+  const imageURL = new URL('../assets/images/image1.png', import.meta.url).href;
+  const icon1URL = new URL('../assets/icons/icon1.svg', import.meta.url).href;
+  const icon2URL = new URL('../assets/icons/icon2.svg', import.meta.url).href;
 
   return (
     <div className="app">
-      {/* <img src={imageURL} width="50" alt="hello" />
+      <img src={imageURL} width="50" alt="hello" />
       <img src={icon1URL} width="50" alt="hello" />
-      <img src={icon2URL} width="50" alt="hello" /> */}
+      <img src={icon2URL} width="50" alt="hello" />
       <h1>
         <ul className="list">
           <li>+ TypeScript</li>
