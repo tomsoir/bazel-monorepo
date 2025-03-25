7f236f166b02d1fce832eed341d50b26c058fecc
artem <tomsoir@gmail.com>
2025-03-25 01:13:03 -0700

    Prepare for infra integration

---
+++ experimental/service_test_ts_infra/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import hello from './utils.js';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const text = hello() || 'OOPS BACKUP "HELLO WORLD"';
+  res.send(text);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ experimental/service_test_ts_infra/src/utils.ts
@@ -0,0 +1,4 @@
+export default function hello() {
+  console.log(2);
+  return 'HELLO WORLD!';
+}
