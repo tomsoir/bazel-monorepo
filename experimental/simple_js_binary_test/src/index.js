e0162127a34c75d90c7f949ed4c6a7803333badd
artem <tomsoir@gmail.com>
2025-04-07 16:05:04 -0700

    Add simple js_binary example with internal deps

---
+++ experimental/simple_js_binary_test/src/index.js
@@ -0,0 +1,13 @@
+import cowsay from 'cowsay';
+import file1JSON from './file1.json' assert { type: 'json' };
+import file22JSON from './file22.json' assert { type: 'json' };
+
+const cowWords = cowsay.say({
+  text: "I'm a moooodule",
+  e: 'oO',
+  T: 'U ',
+});
+console.log(cowWords);
+
+console.log('files content 1', { file1JSON });
+console.log('files content 22', { file22JSON });
